4b2b0549202d5e963c395a36c241147de74a21fb
olvmike <maicon.olv@gmail.com>
2025-01-23 11:41:56 -0300

    GetById()

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -2,8 +2,6 @@ package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
 
-import jakarta.persistence.Column;
-
 public class GameMinDTO {
 	private Long id;
 	private String title;
